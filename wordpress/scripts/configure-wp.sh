6c80b44e2cbfea6635893405fb2fb20ae7401895
Gonzalo Rafuls <grafuls@redhat.com>
2017-08-23 14:39:12 +0200

    Added jp-markdown plugin
    Change-Id: I357e2728d35c4547e60465a13be4fb0faa9056b4

--- wordpress/scripts/configure-wp.sh
+++ wordpress/scripts/configure-wp.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 wp core install --path=/var/www/html --url=http://localhost --title=Quads --admin_user=admin --admin_password=admin --admin_email=grafuls@redhat.com
+wp plugin install jetpack-markdown --activate --path=/var/www/html
 wp theme install krusze --activate --path=/var/www/html
